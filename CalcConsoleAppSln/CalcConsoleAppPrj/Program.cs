e7902790506cec0ea78311be5e88ac2d48eece01
shandreysh <33205692+shandreysh@users.noreply.github.com>
2017-10-29 22:47:45 +0200

    Add files

---
+++ CalcConsoleAppSln/CalcConsoleAppPrj/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcConsoleAppPrj
+{
+    class Program
+    {
+        static void PrintMenu()
+        {
+            Console.WriteLine("1) Сложить два числа.");
+            Console.WriteLine("2) Вычесть два числа.");
+            Console.WriteLine("...");
+            Console.Write("Выберите действие:");
+        }
+
+        static void AddTwoNumbers()
+        {
+            Console.Write("Введите первое число:");
+            string num1Str;
+            num1Str = Console.ReadLine();
+            Console.Write("Введите второе число:");
+            string num2Str;
+            num2Str = Console.ReadLine();
+
+            //Преобразуем строки в числа
+            double num1;
+            num1 = Convert.ToDouble(num1Str);
+            double num2;
+            num2 = Convert.ToDouble(num1Str);
+
+            //Производим вычиления
+            double res;
+            res = num1 + num2;
+
+            //Отображаем результат
+            Console.WriteLine("Результат сложения - " + res.ToString());
+
+            //Остановка выполнения программы
+            Console.ReadLine();
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculator");
+
+            //Печатаем в консоли меню для пользователя
+            PrintMenu();
+
+            string Action;
+            //Пользователь вводит текст
+            Action = Console.ReadLine();
+
+            //Console.WriteLine("Action is - " + Action);
+            //Console.ReadLine();
+
+            //Принимаем решение
+            if (Action == "1")
+            {
+                AddTwoNumbers();
+            }
+            if (Action == "2")
+            {
+                //SubtractTwoNumbers();
+            }
+            if (Action == "3")
+            {
+                //SubtractTwoNumbers();
+            }
+        }
+    }
+}
---
+++ CalcConsoleAppSln/CalcConsoleAppPrj/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("CalcConsoleAppPrj")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("CalcConsoleAppPrj")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("7cede626-f1d3-4938-8d2f-3446726076f3")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
